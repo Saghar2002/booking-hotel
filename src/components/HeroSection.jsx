849b4f7da00c93abda8c472488783efd840001ba
Saghar2002 <Sagharrigi81@gmail.com>
2025-11-03 14:21:45 +0330

    Fix the HeroSection image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,20 +3,11 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <>
-       <section className='text-3xl font-bold bg-green-900'
-        //  style={{
-        //     width : "100%" ,
-        //     height: "60vh" ,
-        //     display: "flex" ,
-        //     alignItems: "center" , 
-        //     justifyContent: "center" ,
-        //     backgroundColor: "#1a365d",
-        //     color: "#fff" ,
-        //     fontSize: "2rem" ,
-        //     fontWeight: "bold" ,
-        // }}
+       <section className='relative w-full h-[90vh] bg-cover bg-center bg-no-repeat'
+        style={{
+            backgroundImage: `url('/HeroSection.png')`
+        }}
         >
-          Start Your Journey to Your Dream Destination ✈️
         </section>
     </>
   )
