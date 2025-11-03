396ccbe344cd432a3599e033903b9e5e77bf531e
Saghar2002 <Sagharrigi81@gmail.com>
2025-11-03 17:34:21 +0330

    Fixed the header :sparkles:

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,6 +10,21 @@ const HeroSection = () => {
         }}
       >
         <div className="absolute inset-0 bg-black/60 "></div>
+
+        {/* header content */}
+
+        <header className='relative z-10 flex justify-between items-center px-5 py-6 text-white'>
+          <div className='font bold px-4'>October Hotel</div>
+          <nav className='space-x-6 px-5'>
+            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Home</a>
+            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Hotel</a>
+            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Offer</a>
+            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Contact</a>
+          </nav>
+        </header>
+
+
+        {/* hero section */}
         <div className="relative z-10  flex flex-col justify-center items-center h-full text-center text-white px-4">
           <h1 className="sm:text-2xl md:text-4xl font-bold mb-4">
             Luxury & Comfort, Just a Click Away
