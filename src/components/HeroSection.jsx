1db2f4380134b243822ebeae043ca013b7fc44ab
Saghar2002 <Sagharrigi81@gmail.com>
2025-11-03 16:09:55 +0330

    finish the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const HeroSection = () => {
   return (
     <>
-       <section className='relative w-full h-[90vh] bg-cover bg-center bg-no-repeat'
+      <section
+        className="relative w-full h-[100vh] bg-cover bg-center bg-no-repeat"
         style={{
-            backgroundImage: `url('/HeroSection.png')`
+          backgroundImage: `url('/HeroSection.png')`,
         }}
-        >
-        </section>
+      >
+        <div className="absolute inset-0 bg-black/60 "></div>
+        <div className="relative z-10  flex flex-col justify-center items-center h-full text-center text-white px-4">
+          <h1 className="sm:text-2xl md:text-4xl font-bold mb-4">
+            Luxury & Comfort, Just a Click Away
+          </h1>
+          <p className="sm:text-lg md:text-2xl font-light">
+           Best prices, best comfort, all in one place
+          </p>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
