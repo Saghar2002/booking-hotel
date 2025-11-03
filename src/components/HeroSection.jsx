84ceb9fb5776cbb6f11dac89800ad70f7c95b8f1
Saghar2002 <Sagharrigi81@gmail.com>
2025-11-03 23:26:26 +0330

    make the header responsive :sparkles:

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HeroSection = () => {
+  const [openMenu, setOpenMenu] = useState("false");
+
   return (
     <>
       <section
@@ -13,16 +15,62 @@ const HeroSection = () => {
 
         {/* header content */}
 
-        <header className='relative z-10 flex justify-between items-center px-5 py-6 text-white'>
-          <div className='font bold px-4'>October Hotel</div>
-          <nav className='space-x-6 px-5'>
-            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Home</a>
-            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Hotel</a>
-            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Offer</a>
-            <a href="#" className='inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded'>Contact</a>
+        <header className="relative z-10 flex justify-between items-center px-5 py-6 text-white">
+          <div className="font bold px-4">October Hotel</div>
+          <nav className="hidden md:flex space-x-6 px-5">
+            <a
+              href="#"
+              className="inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded"
+            >
+              Hotel
+            </a>
+            <a
+              href="#"
+              className="inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded"
+            >
+              Offer
+            </a>
+            <a
+              href="#"
+              className="inline-block px-4 py-2 text-center font-semibold transition-all duration-200 hover:drop-shadow hover:bg-white/10 rounded"
+            >
+              Contact
+            </a>
           </nav>
+
+          {/* Mobile hamburg menu */}
+          <button
+            className="md:hidden text-2xl"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            ☰
+          </button>
         </header>
 
+        {/* mobile menu */}
+
+        {openMenu && (
+          <nav className="md:hidden absolute z-20 top-0left-0 w-full bg-black/80 flex flex-col items-center space-x-4 py-6 text-white">
+            <a href="#" className="w-full text-center py-2 hover: bg-white/20">
+              Home
+            </a>
+            <a href="#" className="w-full text-center py-2 hover: bg-white/20">
+              Hotel
+            </a>
+            <a href="#" className="w-full text-center py-2 hover: bg-white/20">
+              Offer
+            </a>
+            <a href="#" className="w-full text-center py-2 hover: bg-white/20">
+              Contact
+            </a>
+          </nav>
+        )}
 
         {/* hero section */}
         <div className="relative z-10  flex flex-col justify-center items-center h-full text-center text-white px-4">
