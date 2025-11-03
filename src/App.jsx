76f803dcf57bf3ba5504cb58da36c66427ff231c
Saghar2002 <Sagharrigi81@gmail.com>
2025-11-03 12:58:02 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './design/main.css'
+import Hero from './components/hero'
+
+const App = () => {
+  return (
+    <>
+     <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <section className='text-3xl font-bold bg-red-800'
+        //  style={{
+        //     width : "100%" ,
+        //     height: "60vh" ,
+        //     display: "flex" ,
+        //     alignItems: "center" , 
+        //     justifyContent: "center" ,
+        //     backgroundColor: "#1a365d",
+        //     color: "#fff" ,
+        //     fontSize: "2rem" ,
+        //     fontWeight: "bold" ,
+        // }}
+        >
+          Start Your Journey to Your Dream Destination ✈️
+        </section>
+    </>
+  )
+}
+
+export default Hero
